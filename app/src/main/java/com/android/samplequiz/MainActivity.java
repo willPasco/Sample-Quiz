27bf1ba6979579399219a352b651fd61c4e2c1f6
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-04-16 13:25:45 -0300

    Testing architecture flow.

--- app/src/main/java/com/android/samplequiz/MainActivity.java
+++ app/src/main/java/com/android/samplequiz/MainActivity.java
@@ -1,13 +1,29 @@
 package com.android.samplequiz;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.samplequiz.viewmodel.QuestionViewModel;
+
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    ViewModelProvider.Factory factory;
+
+    QuestionViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppApplication.getComponent().inject(this);
+
+        viewModel = ViewModelProviders.of(this, factory).get(QuestionViewModel.class);
+        viewModel.loadQuestion();
     }
 }
