6b7c20d73602ede7249b2842a5ddf0cad264f42a
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-04-16 13:25:29 -0300

    Making main component be singleton.

--- app/src/main/java/com/android/samplequiz/di/MainComponent.java
+++ app/src/main/java/com/android/samplequiz/di/MainComponent.java
@@ -2,12 +2,15 @@ package com.android.samplequiz.di;
 
 import com.android.samplequiz.MainActivity;
 
+import javax.inject.Singleton;
+
 import dagger.Component;
 
 @Component(modules = {ModelModule.class,
         ViewModelModule.class,
         RepositoryModule.class,
         NetworkModule.class})
+@Singleton
 public interface MainComponent {
 
     void inject(MainActivity mainActivity);
