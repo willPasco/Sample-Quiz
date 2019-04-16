fcf34a27ba0be51387495564072fea8eed589e78
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-04-16 12:20:32 -0300

    Pushing initial project.

---
+++ app/src/main/java/com/android/samplequiz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.samplequiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
