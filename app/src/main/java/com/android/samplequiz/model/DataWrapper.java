7ab746fff774aa6e82043bb5d7e53c4e755472f6
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-04-16 13:23:18 -0300

    Creating pojos to api request.

---
+++ app/src/main/java/com/android/samplequiz/model/DataWrapper.java
@@ -0,0 +1,29 @@
+package com.android.samplequiz.model;
+
+import javax.inject.Inject;
+
+public class DataWrapper<T> {
+
+    private T data;
+    private int code;
+
+    @Inject
+    public DataWrapper() {
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
---
+++ app/src/main/java/com/android/samplequiz/model/Question.java
@@ -0,0 +1,46 @@
+package com.android.samplequiz.model;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class Question {
+
+    private int id;
+    private String statement;
+    private List<String> options;
+
+    @Inject
+    public Question() {
+    }
+
+    public Question(int id, String statement, List<String> options) {
+        this.id = id;
+        this.statement = statement;
+        this.options = options;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatement() {
+        return statement;
+    }
+
+    public void setStatement(String statement) {
+        this.statement = statement;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+}
