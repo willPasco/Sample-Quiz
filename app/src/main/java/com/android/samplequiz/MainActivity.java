b6901253185fd5bef14c2a87e9f18ad882689559
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-04-16 21:21:53 -0300

    Adding different states in question activity.

--- app/src/main/java/com/android/samplequiz/MainActivity.java
+++ app/src/main/java/com/android/samplequiz/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -32,9 +33,14 @@ import static com.android.samplequiz.service.QuizService.REQUEST_OK;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static final int LOAD_QUESTION = 0;
+    private static final int GET_ANSWER = 1;
 
     @ViewById(R.id.button_action)
     Button buttonAction;
+
+    @ViewById(R.id.button_error)
+    Button buttonError;
 
     @ViewById(R.id.question_radio_group)
     RadioGroup questionRadioGroup;
@@ -42,10 +48,20 @@ public class MainActivity extends AppCompatActivity {
     @ViewById(R.id.text_view_question)
     TextView questionTextView;
 
+    @ViewById(R.id.text_view_error)
+    TextView errorTextView;
+
+    @ViewById(R.id.text_view_initial)
+    TextView initTextView;
+
+    @ViewById(R.id.progress_bar)
+    ProgressBar progressBar;
+
     @Inject
     ViewModelProvider.Factory factory;
 
     QuestionViewModel viewModel;
+    private int questionId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,27 +80,93 @@ public class MainActivity extends AppCompatActivity {
                 if (dataWrapper != null) {
                     if (dataWrapper.getCode() == REQUEST_OK) {
                         if (dataWrapper.getData().getOptions() != null) {
-                            showContentState();
                             configView(dataWrapper.getData());
+                            showContentState();
                         }
                     } else {
                         showErrorState();
+                        configErrorButton(LOAD_QUESTION, 0);
                         Log.e(TAG, String.valueOf(dataWrapper.getCode()));
                     }
                 } else {
                     Log.e(TAG, "DataWrapper is null");
                     showErrorState();
+                    configErrorButton(LOAD_QUESTION, 0);
+                }
+            }
+        });
+
+        viewModel.getAnswerLivedata().observe(MainActivity.this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(@Nullable Boolean result) {
+                if (result != null) {
+                    checkAnswer(result);
+                    enableNextQuestionButton();
+                    showContentState();
+                } else {
+                    showErrorState();
+                    configErrorButton(GET_ANSWER, questionId);
                 }
             }
         });
     }
 
+    private void configErrorButton(int typeListener, final int questionId) {
+
+        switch (typeListener) {
+
+            case LOAD_QUESTION:
+                buttonError.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showContentState();
+                        loadNewQuestion();
+                    }
+                });
+                break;
+            case GET_ANSWER:
+                buttonError.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showContentState();
+                        loadAnswer(questionId);
+                    }
+                });
+                break;
+        }
+    }
+
+    private void showLoadingState() {
+
+        questionTextView.setVisibility(View.GONE);
+        questionRadioGroup.setVisibility(View.GONE);
+        buttonAction.setVisibility(View.GONE);
+        initTextView.setVisibility(View.GONE);
+        buttonError.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
     private void showContentState() {
 
+        questionTextView.setVisibility(View.VISIBLE);
+        questionRadioGroup.setVisibility(View.VISIBLE);
+        buttonAction.setVisibility(View.VISIBLE);
+        initTextView.setVisibility(View.GONE);
+        buttonError.setVisibility(View.GONE);
+        errorTextView.setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
     }
 
     private void showErrorState() {
 
+        questionTextView.setVisibility(View.GONE);
+        questionRadioGroup.setVisibility(View.GONE);
+        buttonAction.setVisibility(View.GONE);
+        initTextView.setVisibility(View.GONE);
+        buttonError.setVisibility(View.VISIBLE);
+        errorTextView.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.GONE);
     }
 
     private void configView(Question data) {
@@ -94,16 +176,15 @@ public class MainActivity extends AppCompatActivity {
         createRadioButtons(optionsList);
 
         Log.i(TAG, data.toString());
-        enableAnswerButton(28);
+        enableAnswerButton(data.getId());
 
     }
 
-
     private void createRadioButtons(List<String> optionsList) {
-        for (String option : optionsList) {
+        for (int i = 0; i < optionsList.size(); i++) {
             RadioButton radioButton = new RadioButton(this);
-            radioButton.setText(option);
-            radioButton.setId(option.length());
+            radioButton.setText(optionsList.get(i));
+            radioButton.setId(i);
             radioButton.setPadding(0, 16, 0, 0);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
                 radioButton.setGravity(View.TEXT_ALIGNMENT_CENTER);
@@ -115,25 +196,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void enableAnswerButton(final int id) {
 
-        buttonAction.setText("answer");
-        buttonAction.setVisibility(View.VISIBLE);
+        buttonAction.setText(getString(R.string.answer));
+        loadAnswer(id);
+    }
+
+    private void loadAnswer(final int id) {
+        questionId = id;
         buttonAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewModel.getAnswer("6:20", id).observe(MainActivity.this, new Observer<Boolean>() {
-                    @Override
-                    public void onChanged(@Nullable Boolean result) {
-                        if (result != null) {
-
-                            checkAnswer(result);
-
-                            enableNextQuestionButton();
-                            Log.i(TAG, String.valueOf(result));
-                        }
-                    }
-                });
+                showLoadingState();
+                String value = getRadioCheckedValue();
+                viewModel.loadAnswer(value, id);
             }
         });
+    }
+
+    private String getRadioCheckedValue() {
+        int id = questionRadioGroup.getCheckedRadioButtonId();
+        RadioButton radioButton = questionRadioGroup.findViewById(id);
+        return radioButton.getText().toString();
     }
 
     private void checkAnswer(Boolean result) {
@@ -149,10 +231,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void enableNextQuestionButton() {
-        buttonAction.setText("Next");
+        buttonAction.setText(getString(R.string.nest_question));
         buttonAction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                showLoadingState();
                 clearRadioGroup();
                 viewModel.loadQuestion();
             }
@@ -160,6 +243,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void clearRadioGroup() {
+        questionRadioGroup.clearCheck();
         int count = questionRadioGroup.getChildCount();
         for (int i = 0; i < count; i++) {
             questionRadioGroup.removeViewAt(0);
@@ -168,6 +252,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void startQuiz(View view) {
         view.setVisibility(View.GONE);
+        showLoadingState();
         loadNewQuestion();
     }
 
