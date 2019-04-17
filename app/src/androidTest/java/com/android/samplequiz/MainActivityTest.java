15737f826b1e1b5adc4f543a09a6d12d9b637c81
William Pasco de Azevedo <will.pasco.az@gmail.com>
2019-04-17 13:52:19 -0300

    Creating test to question state.

--- app/src/androidTest/java/com/android/samplequiz/MainActivityTest.java
+++ app/src/androidTest/java/com/android/samplequiz/MainActivityTest.java
@@ -1,8 +1,33 @@
 package com.android.samplequiz;
 
+import android.content.ComponentName;
+import android.support.test.espresso.IdlingRegistry;
+import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.RadioButton;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -43,3 +68,32 @@ public class MainActivityTest {
         onView(withId(R.id.button_start)).perform(click());
         onView(withText(R.string.user_empty)).inRoot(withDecorView(not(is(activityIntentsTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void clickAnswerQuestion_WithoutSelectOption() {
+        onView(withId(R.id.edit_text_user_name)).perform(typeText("User test"));
+        closeSoftKeyboard();
+        onView(withId(R.id.button_start)).perform(click());
+        onView(withId(R.id.question_radio_group)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.button_action)).perform(click());
+
+        onView(withText(R.string.radio_not_selected)).inRoot(withDecorView(not(is(activityIntentsTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void clickAnswerQuestion_WithSelectedOption() {
+        onView(withId(R.id.edit_text_user_name)).perform(typeText("User test"));
+        closeSoftKeyboard();
+        onView(withId(R.id.button_start)).perform(click());
+        onView(withId(R.id.question_radio_group)).check(matches(isDisplayed()));
+
+        RadioButton view = (RadioButton) activityIntentsTestRule.getActivity().questionRadioGroup.getChildAt(2);
+        view.setChecked(true);
+
+        onView(withId(R.id.button_action)).perform(click());
+
+        onView(withId(view.getId())).check(matches(not(isEnabled())));
+    }
+
+}
